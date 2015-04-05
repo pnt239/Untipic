af847355c6aabdc53db79e86a1167c9c3b78b63a
pnt239 <pnt239@yahoo.com>
2015-04-05 23:26:24 +0700

    Build Metro for window

--- Untipic/Form1.cs
+++ Untipic/Form1.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace Untipic
 {
-    public partial class Form1 : Form
+    public partial class Form1 : Untipic.UI.Net.MetroUI.MetroForm
     {
         public Form1()
         {
