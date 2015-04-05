1f03494480969c3b3ba3ca975372caab8d879128
pnt239 <pnt239@yahoo.com>
2015-04-06 03:56:38 +0700

    Create MainForm for mono

--- Untipic.UI.Mono/MainForm.Designer.cs
+++ Untipic.UI.Mono/MainForm.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Text = "MainForm";
         }
 
         #endregion
