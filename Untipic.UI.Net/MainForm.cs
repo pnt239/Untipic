7935ab7916a8a926ba0a87b5d535ee0f51e59764
pnt239 <pnt239@yahoo.com>
2015-04-06 04:01:32 +0700

    Add main form for net

--- Untipic.UI.Net/MainForm.cs
+++ Untipic.UI.Net/MainForm.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Untipic.UI.Net
 {
-    public partial class MainForm : Untipic.UI.Net.MetroUI.MetroForm
+    public partial class MainForm : Form
     {
         public MainForm()
         {
