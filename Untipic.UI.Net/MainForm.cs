a88c067df164b47dde5e6c4b9f6a4a82b3bc3956
pnt239 <pnt239@yahoo.com>
2015-04-06 23:56:33 +0700

    Add MainForm

---
+++ Untipic.UI.Net/MainForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Untipic.UI.Net
+{
+    public partial class MainForm : Untipic.UI.Net.UntiUI.UntiForm
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
