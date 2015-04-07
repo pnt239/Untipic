7af7707f3d9699822635ab4570805b6216d5add8
pnt239 <pnt239@yahoo.com>
2015-04-08 05:04:01 +0700

    Add image obj

---
+++ Untipic.Entity/ImageObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+
+namespace Untipic.Entity
+{
+    public class ImageObject : IDrawingObject
+    {
+        public PointF Location { get; set; }
+        public SizeF Size { get; set; }
+        public DrawingObjectType GetObjectType()
+        {
+            return DrawingObjectType.Image;
+        }
+    }
+}
