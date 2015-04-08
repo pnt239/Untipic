2e8122d174db1a6179c3fc69e65bfb52b785c58d
panoti <pano.tjoan@yahoo.com>
2015-04-08 13:43:16 +0700

    * Untipic.csproj: * MainForm.cs: * gui.stetic: * generated.cs: * Untipic.UI.Mono.csproj: * AssemblyInfo.cs: * Untipic.UI.Mono.MainForm.cs:
    * Untipic.sln: Add ui for mono use gtk

---
+++ Untipic.UI.Mono/MainForm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Gtk;
+
+namespace Untipic.UI.Mono
+{
+	public partial class MainForm : Gtk.Window
+	{
+		public MainForm () :
+			base (Gtk.WindowType.Toplevel)
+		{
+			this.Build ();
+		}
+	}
+}
+
