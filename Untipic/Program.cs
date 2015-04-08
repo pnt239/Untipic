7d41b7c3f16f3799bf7f2dde5f5810175e3d10de
pnt239 <pnt239@yahoo.com>
2015-04-08 14:36:15 +0700

    config hist

--- Untipic/Program.cs
+++ Untipic/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Untipic.UI;
 
 namespace Untipic
 {
@@ -15,19 +16,9 @@ namespace Untipic
         [STAThread]
         static void Main()
         {
-#if MONO
-			Assembly assembly = Assembly.LoadFrom("Untipic.UI.Mono.dll");
-            Type type = assembly.GetType("Untipic.UI.Mono.MainForm");
-#else
-			Assembly assembly = Assembly.LoadFrom("Untipic.UI.dll");
-            Type type = assembly.GetType("Untipic.UI.MainForm");
-#endif
-
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-
-            Form mainForm = (Form)Activator.CreateInstance(type);
-            Application.Run(mainForm);
+            Application.Run(new MainForm());
         }
     }
 }
