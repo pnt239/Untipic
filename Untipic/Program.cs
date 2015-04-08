40b71c974ff56ee8bddca96ab4dd91f75abf0f3f
pnt239 <pnt239@yahoo.com>
2015-04-08 13:52:50 +0700

    config for gtk UI

--- Untipic/Program.cs
+++ Untipic/Program.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Untipic.UI;
 
 namespace Untipic
 {
@@ -16,9 +15,18 @@ namespace Untipic
         [STAThread]
         static void Main()
         {
+            Assembly assembly = Assembly.LoadFrom("Untipic.UI.dll");
+#if MONO
+            Type type = assembly.GetType("Untipic.UI.Mono.MainForm");
+#else
+            Type type = assembly.GetType("Untipic.UI.MainForm");
+#endif
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+
+            Form mainForm = (Form)Activator.CreateInstance(type);
+            Application.Run(mainForm);
         }
     }
 }
